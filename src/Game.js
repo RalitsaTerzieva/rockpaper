d8b9a630cf8be6db006db1e67ba6bced1ee5bcc2
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2021-06-27 16:33:28 +0300

    Implement game logic
    * Implement gameplay logic
    * Add a new game button

--- src/Game.js
+++ src/Game.js
@@ -6,20 +6,55 @@ import Button from './Button.js';
 class Game extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.initialState = {
+            round: 1,
+            wins: 0,
+            losses: 0,
+            ties: 0,
+            playerChoice: "rock",
+            aiChoice: "rock",
+        };
+        this.state = this.initialState
+    }
+
+    buttonClicked = (playerChoice) => {
+        let choices = ['rock', 'paper', 'scissors']
+        let index = Math.floor(Math.random() * choices.length);
+        let aiChoice = choices[index];
+
+        let newState = {
+            ...this.state,
+            'playerChoice': playerChoice,
+            'aiChoice': aiChoice,
+            'round': this.state['round'] + 1,
+        }
+
+        if (playerChoice === aiChoice) {
+            newState['ties']++
+        } else if (
+            (playerChoice === "rock" && aiChoice === "scissors") ||
+            (playerChoice === "paper" && aiChoice === "rock") ||
+            (playerChoice === "scissors" && aiChoice === "paper")
+        ) {
+            newState['wins']++
+        } else {
+            newState['losses']++
+        }
+
+        this.setState(newState)
     }
     
-    buttonClicked(choice) {
-        console.log(choice)
+    reset = () => {
+        this.setState(this.initialState);
     }
 
     render() {
         return <table className="game-table">
             <tbody>
                 <tr>
-                    <td><Choice value="rock" orientation="right" player="Player" /></td>
+                    <td><Choice value={this.state.playerChoice} orientation="right" player="Player" /></td>
                     <td style={{textAlign: "center"}}>
-                        <h1 className="counter">Round 6</h1>
+                        <h1 className="counter">Round {this.state.round}</h1>
                         <div className="buttons">
                             <Button value="rock" onClick={this.buttonClicked} />
                             <Button value="paper" onClick={this.buttonClicked} />
@@ -27,15 +62,16 @@ class Game extends Component {
                         </div>
                         <div>
                             <hr />
-                            <strong>Wins: </strong><span className="counter" id="wins">0</span>
+                            <strong>Wins: </strong><span className="counter" id="wins">{this.state.wins}</span>
                             <hr />
-                            <strong>Ties: </strong><span className="counter" id="ties">5</span>
+                            <strong>Ties: </strong><span className="counter" id="ties">{this.state.ties}</span>
                             <hr />
-                            <strong>Losses: </strong><span className="counter" id="losses">10</span>
+                            <strong>Losses: </strong><span className="counter" id="losses">{this.state.losses}</span>
                             <hr />
+                            <button onClick={this.reset} className="start-over">New Game</button>
                         </div>
                     </td>
-                    <td><Choice value="paper" orientation="left" player="AI" /></td>
+                    <td><Choice value={this.state.aiChoice} orientation="left" player="AI" /></td>
                 </tr>
             </tbody>
         </table>
